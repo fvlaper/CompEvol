6aae18aff71cba5ad07dad60d57efb38eb3e4ca0
F. V. Laper <fvlaper@gmail.com>
2015-04-29 16:43:03 -0300

    Implementação das funções de fitness.
    Implementa funções de fitness por inversão e deslocamento, além da
    função auxiliar para transformar o problema original em um problema
    irrestrito. Inclui o artigo escrito para o trabalho TP1.

--- TP2/Flavio.m
+++ TP2/Flavio.m
@@ -17,11 +17,12 @@ function [ x, f, g, h ] = Flavio( ncal, nvar )
 pop = popinit(3,3,-5.12,5.12);
 
 x = [1,1,1];
-f = 2;
-g = pop;
-%h = rastrigin(pop);
+f = pop;
+%g = rastrigin(pop);
+g = fmod(@rastrigin, @rastrigin_le, @rastrigin_eq, 10, 10, pop);
+h = fitness_desc(@rastrigin, @rastrigin_le, @rastrigin_eq, 10, 10, 1.2, pop);
 %h = restr_desig(pop);
-h = restr_ig(pop);
+%h = rastrigin_eq(pop);
 
 end
 
@@ -43,6 +44,76 @@ pop = xmin * ones(npop,nvar) + (xmax - xmin) * rand(npop,nvar);
 
 end
 
+function [d] = fitness_inv(f, gle, geq, r, s, n, pop)
+%FITNESS_INV Função de fitness pelo método de inversão.
+%   Calcula a fitness dos indivíduos da população pelo método de
+%   inversão (transformando um problema de minimização em maximização).
+%
+%   Parâmetros de entrada:
+%       - f: handle da função de otimização original;
+%       - gle: handle da função de restrição de desigualdade;
+%       - geq: handle da função de restrição de igualdade;
+%       - r: parâmetro de penalidade para a restrição de desigualdade;
+%       - s: parâmetro de penalidade para a restrição de igualdade;
+%       - n: parâmetro de escalonamento para a inversão;
+%       - pop: array contendo a polulação (um indivíduo por linha).
+%
+%   Parâmetros de saída:
+%       - d: valor da fitness para cada indivíduo (vetor coluna).
+
+h = fmod(f, gle, geq, r, s, pop);
+d = 1 ./ (h - (min(h) - (10 ^ -n)));
+end
+
+function [d] = fitness_desc(f, gle, geq, r, s, k, pop)
+%FITNESS_DESC Função de fitness pelo método de deslocamento.
+%   Calcula a fitness dos indivíduos da população pelo método de
+%   deslocamento (transformando um problema de minimização em maximização).
+%
+%   Parâmetros de entrada:
+%       - f: handle da função de otimização original;
+%       - gle: handle da função de restrição de desigualdade;
+%       - geq: handle da função de restrição de igualdade;
+%       - r: parâmetro de penalidade para a restrição de desigualdade;
+%       - s: parâmetro de penalidade para a restrição de igualdade;
+%       - k: parâmetro de escalonamento para o deslocamento;
+%       - pop: array contendo a polulação (um indivíduo por linha).
+%
+%   Parâmetros de saída:
+%       - d: valor da fitness para cada indivíduo (vetor coluna).
+
+h = fmod(f, gle, geq, r, s, pop);
+cmax = k * sum(h) / size(h,1);
+display(cmax);
+d = max(zeros(size(h)), (cmax-h));
+end
+
+function [h] = fmod (f, gle, geq, r, s, pop)
+%FMOD Função de otimização modificada.
+%   Calcula o valor da função de otimização (por indivíduo) modificada
+%   para um problema irrestrito.
+%
+%   Parâmetros de entrada:
+%       - f: handle da função de otimização original;
+%       - gle: handle da função de restrição de desigualdade;
+%       - geq: handle da função de restrição de igualdade;
+%       - r: parâmetro de penalidade para a restrição de desigualdade;
+%       - s: parâmetro de penalidade para a restrição de igualdade;
+%       - pop: array contendo a polulação (um indivíduo por linha).
+%
+%   Parâmetros de saída:
+%       - h: valor da função de otimização modificada
+%         para cada indivíduo (vetor coluna).
+
+h = f(pop) ...                                             % func. original
+  + r * sum((max(zeros(size(pop)), gle(pop)) .^ 2) ,2) ... % desigualdades
+  + s * sum(geq(pop) .^ 2, 2);                             % igualdades
+end
+
+%------------------------------------------------------------------------%
+% Função de Rastrigin e restrições
+%------------------------------------------------------------------------%
+
 function [f] = rastrigin (pop)
 %RASTRIGIN Função de Rastrigin.
 %   Calcula a função de Rastrigin para todos os indivíduos da população.
@@ -58,8 +129,8 @@ f = 10 * size(pop,2) + sum(pop .^ 2 - 10 * cos(2 * pi * pop), 2);
 
 end
 
-function [g] = restr_desig (pop)
-%RESTR_DESIG Função de restrição de desigualdade.
+function [g] = rastrigin_le (pop)
+%RASTRIGIN_LE Função de restrição de desigualdade - Rastrigin.
 %   Calcula o valor da restrição de desigualdade para cada variável
 %   de todos os indivíduos da população.
 %
@@ -74,8 +145,8 @@ g = sin(2 * pi * pop) + 0.5;
 
 end
 
-function [h] = restr_ig (pop)
-%RESTR_IG Função de restrição de igualdade.
+function [h] = rastrigin_eq (pop)
+%RASTRIGIN_EQ Função de restrição de igualdade - Rastrigin.
 %   Calcula o valor da restrição de igualdade para cada variável
 %   de todos os indivíduos da população.
 %
