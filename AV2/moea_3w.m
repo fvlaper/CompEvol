6fa67676c70ba8b2ea4ef3001598305dd5d54f6c
F. V. Laper <fvlaper@gmail.com>
2015-06-03 16:54:22 -0300

    Implementa cálculo da fronteira Pareto.
    Implementa cálculo da fronteira Pareto (em andamento)

--- AV2/moea_3w.m
+++ AV2/moea_3w.m
@@ -14,12 +14,19 @@ function [ ps ] = moea_3w( ncal, nvar, no )
 % Layout de array de população: array contendo uma linha por
 % indivíduo. Para cada linha:
 %   - Variáveis de decisão: nvar colunas.
+%   - Funções objetivo: no colunas.
+%   - Valor agregado: uma coluna.
+%   - Fronteira de Pareto: uma coluna.
 xi = 1; xf = xi+nvar-1;
 fi = xf+1; ff = fi+no-1;
-nc = ff;
+ag = ff+1;
+pt = ag+1;
+nc = pt;
 
-L.COLX = xi:xf;
-L.COLF = fi:ff;
+L.COLX  = xi:xf;
+L.COLF  = fi:ff;
+L.COLAG = ag;
+L.COLPT = pt;
 L.NC = nc;
 
 % Dados iniciais da população: faixa das variáveis e 
@@ -31,8 +38,10 @@ npop = 10;
 pop = popinit(npop,xmin,xmax,L);
 
 % Teste
-%pop = dtlz1(pop,nvar,no,L);
-pop = dtlz2(pop,nvar,no,L);
+pop = dtlz1(pop,nvar,no,L);
+%pop = dtlz2(pop,nvar,no,L);
+
+pop = pareto(pop,L);
 
 % Retorno do resultado
 ps = pop;
@@ -57,6 +66,60 @@ pop = zeros(npop, L.NC);
 % Inicializa as variáveis.
 pop(:,L.COLX) = xmin * ones(npop,max(L.COLX)) + ...
                 (xmax - xmin) * rand(npop,max(L.COLX));
+end
+
+function pop = pareto(pop,L)
+%PARETO Calcula a fronteira de Pareto.
+%   Para cada indivíduo da polulação, calcula a que fronteira de
+%   Pareto ele pertence.
+%   Inspirado em XXX.
+%
+%   Parâmetros de entrada:
+%     - npop: número de indivíduos da população;
+%     - xmin: valor mínimo de uma variável;
+%     - xmax: valor máximo de uma variável;
+%     - L: layout de um indivíduo.
+%
+%   Parâmetros de saída:
+%     - pop: array contendo a população inicial.
+
+npop = size(pop,1); % número de indivíduos
+no = max(L.COLF) - min(L.COLF) + 1; % número de funções objetivo
+f = 1;  % f-ésima fronteira de Pareto
+front(f).ids = []; % índices dos indivíduos na i-ésima fronteira
+dominacao = []; % informações de dominação de cada indivíduo.
+
+% Compara cada indivíduo com todos os demais para determinar
+% a dominação.
+for i = 1:npop
+    dominacao(i).n = 0;  % número de indivíduos que dominam i.
+    dominacao(i).d = []; % índices dos indivíduos dominados por i.
+    
+    for j = 1:npop
+        if i ~= j
+            % Compara indivíduos i e j
+            %display(['Comparando ', num2str(i), ' e ' num2str(j)]);
+            difs = pop(i,L.COLF) - pop(j,L.COLF);
+            menores = sum(difs <  0);
+            iguais  = sum(difs == 0);
+            maiores = sum(difs >  0);
+            %display(['menor = ', num2str(menores), ' maior =  ', num2str(maiores), ' igual = ', num2str(iguais)]);
+            
+            if maiores == 0 && iguais ~= no
+                % i domina j: nenhum maior, pelo menos um menor
+                dominacao(i).d = [dominacao(i).d j];
+            elseif menores == 0 && iguais ~= no
+                % j domina i: nenhum menor, pelo menos um diferente
+                dominacao(i).n = dominacao(i).n + 1;
+            end
+        end        
+    end
+end
+
+for i = 1:npop
+    display([num2str(i), ': n = ', dominacao.n, ' d = ', dominacao.d]);
+end
+
 end
 
 function pop = dtlz1 (pop,nvar,no,L)
